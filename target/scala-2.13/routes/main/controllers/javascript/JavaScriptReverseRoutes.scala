6ecd1bd15621a225ca57fa058791a86655b83f29
Ryoko Kawashima <ry.sep27@gmail.com>
2020-08-17 17:10:12 +0900

    first commit

---
+++ target/scala-2.13/routes/main/controllers/javascript/JavaScriptReverseRoutes.scala
@@ -0,0 +1,84 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:/Users/ryoko/app/play-samples-play-scala-hello-world-tutorial 2/conf/routes
+// @DATE:Mon Aug 17 17:03:47 JST 2020
+
+import play.api.routing.JavaScriptReverseRoute
+
+
+import _root_.controllers.Assets.Asset
+
+// @LINE:7
+package controllers.javascript {
+
+  // @LINE:7
+  class ReverseHomeController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:10
+    def hello: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.hello",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "hello"})
+        }
+      """
+    )
+  
+    // @LINE:7
+    def index: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.index",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + """"})
+        }
+      """
+    )
+  
+    // @LINE:9
+    def tutorial: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.tutorial",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "tutorial"})
+        }
+      """
+    )
+  
+    // @LINE:8
+    def explore: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.explore",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "explore"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:13
+  class ReverseAssets(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:13
+    def versioned: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Assets.versioned",
+      """
+        function(file1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "assets/" + (""" + implicitly[play.api.mvc.PathBindable[Asset]].javascriptUnbind + """)("file", file1)})
+        }
+      """
+    )
+  
+  }
+
+
+}
